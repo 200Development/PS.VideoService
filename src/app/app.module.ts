37945024ebfff55c45183b8794544286e987ae04
Ryan Parsons <ryan.parsons@dts-global.com>
2021-07-17 12:20:02 -0700

    Azure upload via web service implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UploadComponent } from './upload/upload.component';
@@ -13,8 +13,9 @@ import { NavigationComponent } from './navigation/navigation.component';
     NavigationComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { HttpEventType, HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-upload',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
+  progress: number | undefined;
+  message: string | undefined;
 
-  constructor() { }
+  @Output() public onUploadFinished = new EventEmitter();
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  public uploadFile = (files) => {
+    if (files.length === 0) {
+      return;
+    }
+
+    let fileToUpload = <File>files[0];
+    const formData = new FormData();
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Access-Control-Allow-Origin':  'true'
+      }),
+      reportProgress: true,
+    };
+    formData.append('file', fileToUpload);
+
+    this.http.post('https://localhost:5001/api/upload', formData, httpOptions)
+      .subscribe(event => {
+            this.message = 'Upload Successful.';
+        })
+  
+  }
 }
