ca0396b08a159d8316d2163b8278d747a3ee4489
Ryan Parsons <ryan.parsons@dts-global.com>
2021-07-18 21:09:57 -0700

    adds service module for azure & payment services and adds video module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AddAccountComponent } from './add-account/add-account.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AddVideoComponent } from './video/add/add.component';
 
 const routes: Routes = [
   { path: 'welcome', component: AppComponent }, 
   { path: 'home', component: HomeComponent },
   { path: 'account', component: AddAccountComponent },
+  { path: 'add-video', component: AddVideoComponent }, 
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ]
