a401b6ad75060d349614da0b5f5a0117057a6f2d
Ryan Parsons <ryan.parsons@dts-global.com>
2021-07-19 22:23:42 -0700

    adds payment and dashboard components and adds to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddAccountComponent } from './add-account/add-account.component';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { AddVideoComponent } from './video/add/add.component';
 
 const routes: Routes = [
   { path: 'welcome', component: AppComponent }, 
   { path: 'home', component: HomeComponent },
-  { path: 'account', component: AddAccountComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'add-account', component: AddAccountComponent },
   { path: 'add-video', component: AddVideoComponent }, 
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AddAccountComponent } from './add-account/add-account.component';
 import { ServiceModule } from './services/service.module';
+import { PaymentComponent } from './payment/payment.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
     HomeComponent,
-    AddAccountComponent    
+    AddAccountComponent,
+    PaymentComponent,
+    DashboardComponent    
   ],
   imports: [        
     BrowserModule,
